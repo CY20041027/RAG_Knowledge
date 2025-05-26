d738fe169ada3588be4f9e63151ac86c212d9dd4
chu-xingyun-is-still-writing <3339320186@qq.com>
2025-05-26 22:37:40 +0800

    初始版

---
+++ xfg-dev-tech-api/src/main/java/cn/bugstack/xfg/dev/tech/api/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @description
+ * @author ruofengcy
+ * @create 2025-03-01 11:07
+ */
+package cn.bugstack.xfg.dev.tech.api;
---
+++ xfg-dev-tech-trigger/src/main/java/cn/bugstack/xfg/dev/tech/trigger/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @description
+ * @author ruofengcy
+ * @create 2025-03-01 11:08
+ */
+package cn.bugstack.xfg.dev.tech.trigger;
